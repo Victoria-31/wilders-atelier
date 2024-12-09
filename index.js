3eac13bcf90d47f1d3ddbf78e71587d19fe84252
Victoria Fonteny <victoriafonteny@live.fr>
2024-12-09 10:59:25 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+import chalk from 'chalk';
+
+
+console.log(chalk.blue('Hello') + chalk.magenta(' Alex,') + chalk.blueBright(' Lisa,') + chalk.bgCyan(' Duncan,') + "and" + chalk.bgGreenBright(' Julian !'));
